01827ea5caa9836a9f2cb8491c206d9c16ffc622
Thaynara Silva <silvathaynara21@gmail.com>
2018-02-08 18:12:24 +0000

    Add Game Ground and Sky Color
    Signed-off-by: Thaynara Silva <silvathaynara21@gmail.com>

--- Petra The Game/Petra The Game/GameScene.swift
+++ Petra The Game/Petra The Game/GameScene.swift
@@ -9,81 +9,57 @@
 import SpriteKit
 import GameplayKit
 
-class GameScene: SKScene {
+class GameScene: SKScene , SKPhysicsContactDelegate {
+
+    var skyColor = SKColor(displayP3Red: 135/255, green: 206/255, blue: 250/255, alpha: 1)
+    var movingNode = SKNode()
     
-    private var label : SKLabelNode?
-    private var spinnyNode : SKShapeNode?
+    let worldCategory : UInt32 = 1 << 1
+    
+    let ground = SKNode()
+    let groundTexture = SKTexture(imageNamed: "ground")
     
     override func didMove(to view: SKView) {
         
-        // Get label node from scene and store it for use later
-        self.label = self.childNode(withName: "//helloLabel") as? SKLabelNode
-        if let label = self.label {
-            label.alpha = 0.0
-            label.run(SKAction.fadeIn(withDuration: 2.0))
-        }
+        self.anchorPoint = CGPoint(x: 0.5, y: 0.5)
         
-        // Create shape node to use during mouse interaction
-        let w = (self.size.width + self.size.height) * 0.05
-        self.spinnyNode = SKShapeNode.init(rectOf: CGSize.init(width: w, height: w), cornerRadius: w * 0.3)
+        self.physicsWorld.gravity = CGVector(dx: 0.0, dy: -0.5)
+        self.physicsWorld.contactDelegate = self
         
-        if let spinnyNode = self.spinnyNode {
-            spinnyNode.lineWidth = 2.5
-            
-            spinnyNode.run(SKAction.repeatForever(SKAction.rotate(byAngle: CGFloat(Double.pi), duration: 1)))
-            spinnyNode.run(SKAction.sequence([SKAction.wait(forDuration: 0.5),
-                                              SKAction.fadeOut(withDuration: 0.5),
-                                              SKAction.removeFromParent()]))
-        }
-    }
-    
-    
-    func touchDown(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.green
-            self.addChild(n)
-        }
-    }
-    
-    func touchMoved(toPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.blue
-            self.addChild(n)
-        }
-    }
-    
-    func touchUp(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.red
-            self.addChild(n)
-        }
-    }
-    
-    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        if let label = self.label {
-            label.run(SKAction.init(named: "Pulse")!, withKey: "fadeInOut")
-        }
+        self.backgroundColor = skyColor
         
-        for t in touches { self.touchDown(atPoint: t.location(in: self)) }
+        self.addChild(movingNode)
+        
+        createGround()
     }
-    
-    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchMoved(toPoint: t.location(in: self)) }
-    }
-    
-    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
-    }
-    
-    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
-    }
-    
     
     override func update(_ currentTime: TimeInterval) {
         // Called before each frame is rendered
+    }
+    
+    func createGround() {
+        groundTexture.filteringMode = .nearest
+        
+        let moveGround = SKAction.moveBy(x: -groundTexture.size().width * 3.0, y: 0,
+                                       duration: TimeInterval(0.006 * groundTexture.size().width * 3.0))
+        let resetGround = SKAction.moveBy(x: groundTexture.size().width * 3.0, y: 0, duration: 0.00)
+        let moveGroundForever = SKAction.repeatForever(SKAction.sequence([moveGround, resetGround]))
+        
+        for i in 0...3 + Int(self.frame.size.width / (groundTexture.size().width * 3)) {
+            let i = CGFloat(i)
+            let groundSprite = SKSpriteNode(texture: groundTexture)
+            groundSprite.setScale(3.0)
+            groundSprite.anchorPoint = CGPoint(x: 0.5, y: 0.5)
+            groundSprite.position = CGPoint(x: i * groundSprite.size.width, y: -(self.frame.size.height / 2.5))
+            groundSprite.run(moveGroundForever)
+            movingNode.addChild(groundSprite)
+        }
+        
+        ground.position = CGPoint(x: 0, y: groundTexture.size().height)
+        ground.physicsBody = SKPhysicsBody(rectangleOf: CGSize(width: self.frame.size.width,
+                                                               height: groundTexture.size().height * 2.0))
+        ground.physicsBody?.isDynamic = false
+        ground.physicsBody?.categoryBitMask = worldCategory
+        self.addChild(ground)
     }
 }
